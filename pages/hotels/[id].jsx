8b67fa845c3e5b11e4889d63ea7a1fa115060bff
admin <harshkurulkar7@gmail.com>
2024-01-09 16:02:25 +0530

    fixed

--- pages/hotels/[id].jsx
+++ pages/hotels/[id].jsx
@@ -1,42 +1,94 @@
+"use client";
+import Head from "next/head";
 import Image from "next/image";
-import React from "react";
+import Cookies from "js-cookie";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-const SingleHotel = () => {
+const SingleHotel = ({ hotel }) => {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const cookie = Cookies.get("user");
+    if (cookie) {
+      setAuth(true);
+      return;
+    }
+    setAuth(false);
+  }, []);
+
   return (
-    <div className="w-7/12 mx-auto my-10">
-      <Image
-        src={
-          "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?cs=srgb&dl=pexels-pixabay-164595.jpg&fm=jpg"
-        }
-        alt="hotel"
-        width={2000}
-        height={2000}
-        className="w-full h-large-box mb-5 my-5 "
-      />
-      <div className="">
-        <h3 className="text-3xl font-bold">Lorem ipsum dolor sit.</h3>
-        <p className="text-xl my-5 text-justify">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem sed
-          soluta modi iusto sunt laboriosam officia fugit numquam architecto
-          animi.
-        </p>
-        <button className="w-60 h-14 rounded-lg bg-blue-400 text-lg">
-          Price : Rs.4500
-        </button>
-        <p className="text-3xl font-bold my-5">Facilities : </p>
-        <ul className="flex text-xl justify-between">
-          <li>Swimming Pool</li>
-          <li>Dogs</li>
-          <li>Garden</li>
-          <li>Loundry</li>
-          <li>Cricket</li>
-        </ul>
-        <button className="w-60 h-14 rounded-lg bg-red-400 text-lg my-5">
-          Book Now
-        </button>
+    <>
+      <Head>
+        <title>{hotel?.name}</title>
+      </Head>
+      <div className="w-7/12 mx-auto my-10 ">
+        <Image
+          src={hotel?.banner}
+          alt="hotel"
+          width={2000}
+          height={2000}
+          className=" w-full h-large-box my-5"
+        />
+        <div className=" ">
+          <h3 className=" text-3xl font-bold">{hotel?.name}</h3>
+          <p className=" text-xl my-5 text-justify">{hotel?.description}</p>
+          <button className=" w-60 h-14 rounded-lg bg-blue-400 text-lg">
+            Price : &#8377; {hotel?.price}
+          </button>
+          <p className=" text-3xl font-bold my-5">Facilities : </p>
+          <ul className=" flex text-xl justify-between">
+            {hotel
+              ? hotel.facilities?.map((ele) => {
+                  return (
+                    <li
+                      key={ele.name}
+                      className=" mr-10 mb-3 flex items-center"
+                    >
+                      <span>
+                        <Image
+                          src={ele.img}
+                          width={200}
+                          height={200}
+                          className="w-8 h-8 rounded-full"
+                        />
+                      </span>
+                      <span className="ml-5">{ele.name}</span>
+                    </li>
+                  );
+                })
+              : ""}
+          </ul>
+          {auth ? (
+            <Link href={`/payment/${hotel?._id}`}>
+              <button className=" w-60 h-14 rounded-lg bg-red-400 my-5 text-lg">
+                Book Now
+              </button>
+            </Link>
+          ) : (
+            <span className=" text-2xl">
+              Please{" "}
+              <Link href={"/login"} className=" text-blue-500">
+                Log in
+              </Link>{" "}
+              to get new Offers !
+            </span>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
+
+export async function getServerSideProps(ctx) {
+  const res = await fetch(`http://localhost:3000/api/hotels/${ctx.query.id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      hotel: data.hotel,
+    },
+  };
+}
 
 export default SingleHotel;
